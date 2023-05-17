4084d93f1d7aea011310260e40baccf87a4e1fe5
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-05-18 07:25:00 +0800

    Revert "Feat: Added terminal based chat feature"
    This reverts commit c70f677f25b3bfe790cf94e58c78da76e92c82b8, reversing
    changes made to 0c8b593d06e7a166a4c62e12df6c7da9f9b5a134.

--- src/MainCircle.java
+++ src/MainCircle.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.awt.*;
 
 import javax.swing.ImageIcon;
@@ -94,73 +93,3 @@ public class MainCircle {
         // This method is not needed for a main square that cannot move
     }
 }
-=======
-import java.awt.*;
-
-import javax.swing.ImageIcon;
-
-public class MainCircle {
-    private int x;
-    private int y;
-    private boolean isClicked;
-    protected int width = 150;
-	protected int height= 150;
-	protected Image image;
-	
-
-    public MainCircle(int x, int y) {
-        this.x = x;
-        this.y = y;
-        Image imageLocation = new ImageIcon("images//MainCircle.png").getImage();
-    	this.image = imageLocation.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setIsClicked(boolean bool) {
-        this.isClicked = bool;
-    }
-
-    public boolean getIsClicked() {
-        return isClicked;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void draw(Graphics g) {
-//        g.setColor(Color.ORANGE);
-//        g.fillRect(x, y, width, height);
-//        g.drawRect(x, y, width, height);
-        
-        g.drawImage(this.image, x, y, width, height, null);
-    }
-
-    public boolean isMouseInsideMainCircle(int mouseX, int mouseY) {
-        return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
-    }
-
-    public void setNewPosition(int x, int y) {
-        // This method is not needed for a main square that cannot move
-    }
-
-    public void getVector(double newXPos, double newYPos) {
-        // This method is not needed for a main square that cannot move
-    }
-
-    public void move() {
-        // This method is not needed for a main square that cannot move
-    }
-}
->>>>>>> with-chat
